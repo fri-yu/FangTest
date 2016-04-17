3ba8e9d242409a348971f61606ba2d341d5f5fe2
fri-yu <18768860215@163.com>
2016-04-17 20:05:16 +0800

    添加‘订单跟进’列表数据模型

---
+++ src/main/java/com/fang/bean/CustomerFollowUpListModel.java
@@ -0,0 +1,53 @@
+package com.fang.bean;
+
+public class CustomerFollowUpListModel {
+	private String No;
+	private String City;
+	private String RealName;
+	private String Phone;
+	private String ProjName;
+	private String NewCode;
+	private String HuxingInfo;
+	public String getNo() {
+		return No;
+	}
+	public void setNo(String no) {
+		No = no;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getRealName() {
+		return RealName;
+	}
+	public void setRealName(String realName) {
+		RealName = realName;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getProjName() {
+		return ProjName;
+	}
+	public void setProjName(String projName) {
+		ProjName = projName;
+	}
+	public String getNewCode() {
+		return NewCode;
+	}
+	public void setNewCode(String newCode) {
+		NewCode = newCode;
+	}
+	public String getHuxingInfo() {
+		return HuxingInfo;
+	}
+	public void setHuxingInfo(String huxingInfo) {
+		HuxingInfo = huxingInfo;
+	}
+}
--- src/main/java/com/fang/controller/CustomerFollowUpController.java
+++ src/main/java/com/fang/controller/CustomerFollowUpController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
 import com.fang.bean.AjaxJsonBean;
+import com.fang.bean.CustomerFollowUpListModel;
 import com.fang.bean.CustomerFollowUpModel;
 import com.fang.service.CustomerFollowUpService;
 
@@ -40,38 +41,38 @@ public class CustomerFollowUpController {
 	@ResponseBody
 	@RequestMapping("/Search.do")
 	public String search() {
-		model1 m1 = new model1();
-		m1.No = "No2";
-		m1.City = "city2";
-		m1.RealName = "RealName2";
-		m1.Phone = "Phone2";
-		m1.ProjName = "ProjName2";
-		m1.NewCode = "NewCode2";
-		m1.HuxingInfo = "HuxingInfo2";
-		AjaxJsonBean<model1> m = new AjaxJsonBean<model1>();
+		CustomerFollowUpListModel m1 = new CustomerFollowUpListModel();
+		m1.setNo("No2");
+		m1.setCity("city2");
+		m1.setRealName("RealName2");
+		m1.setPhone("Phone2");
+		m1.setProjName("ProjName2");
+		m1.setNewCode("NewCode2");
+		m1.setHuxingInfo("HuxingInfo2");
+		AjaxJsonBean<CustomerFollowUpListModel> m = new AjaxJsonBean<CustomerFollowUpListModel>();
 		m.Message = "Success";
 		m.RowsCount = 1;
-		m.Rows = new ArrayList<model1>();
+		m.Rows = new ArrayList<CustomerFollowUpListModel>();
 		m.Rows.add(m1);
-		System.out.println(JSON.toJSON(m));
 		return JSON.toJSON(m).toString();
 	}
 
 	String string = "{\"RowsCount\":1,\"Message\":\"Success\",\"Rows\":[{\"ID\":3235180,\"No\":1,\"City\":\"德清\",\"MallID\":3590741,\"RealName\":\"55\",\"Phone\":\"187****2836\",\"Phone1\":\"85-FC-C1-E6-66-B8-87-26-FD-F0-21-E9-8B-95-03-8A\",\"ProjName\":\"莫干山观云庄园\",\"state\":\"到访\",\"UpdateTime\":\"2016-03-31 15:44:41\",\"hTime\":\"2016-03-31 15:44:38\",\"MediumFlag\":\"新房帮录入 - 其他\",\"FromText\":\"-\",\"SellerName\":\"张亚林\",\"IsfollowUp\":\"是\",\"Refund_audit_status\":\"-\",\"Tuirengou_audit_status\":\"-\",\"Daofang_audit_status\":\"待审\",\"Renchou_audit_status\":\"-\",\"Rengou_audit_status\":\"-\",\"Qianyue_audit_status\":\"-\",\"NewCode\":2023121656,\"SellerID\":42485984,\"HouseInfo\":\"\",\"LiveName\":\"\",\"TuiGuangName\":\"\",\"userState\":\"待付款\",\"rengouState\":\"--\",\"HuxingInfo\":null,\"CanGiveUp\":\"yes\",\"isHaveLoan\":\"否\",\"IsShowGenjin\":\"true\"}]}";
 
 	// public static void main(String[] args) {
-	// model1 m1 = new model1();
-	// m1.No = "No2";
-	// m1.City = "city2";
-	// m1.RealName = "RealName2";
-	// m1.Phone = "Phone2";
-	// m1.ProjName = "ProjName2";
-	// m1.NewCode = "NewCode2";
-	// m1.HuxingInfo = "HuxingInfo2";
-	// model m = new model();
+	// CustomerFollowUpListModel m1 = new CustomerFollowUpListModel();
+	// m1.setNo("No2");
+	// m1.setCity("city2");
+	// m1.setRealName("RealName2");
+	// m1.setPhone("Phone2");
+	// m1.setProjName("ProjName2");
+	// m1.setNewCode("NewCode2");
+	// m1.setHuxingInfo("HuxingInfo2");
+	// AjaxJsonBean<CustomerFollowUpListModel> m = new
+	// AjaxJsonBean<CustomerFollowUpListModel>();
 	// m.Message = "Success";
-	// m.rowsCount = "1";
-	// m.Rows = new ArrayList<model1>();
+	// m.RowsCount = 1;
+	// m.Rows = new ArrayList<CustomerFollowUpListModel>();
 	// m.Rows.add(m1);
 	// System.out.println(JSON.toJSON(m));
 	// }
